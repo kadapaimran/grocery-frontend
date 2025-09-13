b975a71430d9970da0ac4c7c8f21929e54c6597c
kadapaimran <kadapaimran99@hotmail.com>
2025-09-13 10:10:04 +0530

    vite resolved

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/frontapp1/',   // 👈 must match Tomcat context path
+  base: '/frontapp1/',   // or './' if you prefer relative
+  plugins: [react()]
 })
