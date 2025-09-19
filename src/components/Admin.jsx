4aee2ddcd31d513b76f5e5385a10ea45f2ec8d79
Kadapah Imran <kadapaimran99@hotmail.com>
2025-09-19 15:42:36 +0530

    Axios Error & image error resolved

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../styles/Admin.css";
 
-const BASE_URL = "http://localhost:8088/api/products";
-const DEFAULT_IMAGE = "https://via.placeholder.com/80?text=No+Image";
+const BASE_URL = "http://localhost:9090/springapp1/api/products";
+const DEFAULT_IMAGE = "https://th.bing.com/th/id/OIP.wPjOsacGuRUjiOEUJmhRDgHaHa?w=179&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3";
 
 const Admin = () => {
   const navigate = useNavigate();
